dbb13b0ea4bad0768980c14c282825976e5119e0
Talise Hendrix <trhendri1@gmail.com>
2025-01-26 22:46:52 -0600

    Completed Test Case 1

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -4,39 +4,32 @@ const page = require("../pageobjects/page");
 
 //AutomationExercises.com Project
 
-describe("Test Case 1", () => {
-    it.only("Should Register User", async () => {
-        //Launch Broswer, navigate to site, and confirm homepage is visible
-        await browser.url('/');
-        const browserUrl = 'https://www.automationexercise.com/';
-        await expect (browser).toHaveUrl(browserUrl);
+describe('Test Case 1', () => {
+    it("Should Register User", async () => {
+        //Launch Browser, navigate to site, and confirm homepage is visible
+        await browser.url("/");
+        const browserUrl = "https://www.automationexercise.com/";
+        await expect(browser).toHaveUrl(browserUrl);
 
         //Click 'Signup / Login' button, Verify 'New User Signup!' is visible
         const loginBtn = await $('//a[contains(text(), "Signup / Login")]');
         await loginBtn.click();
-        const newUserSignUpText = await $('.signup-form h2');
+        const newUserSignUpText = await $(".signup-form h2");
         await expect(newUserSignUpText).toBeDisplayed;
-        await expect(newUserSignUpText).toHaveText('New User Signup!');
-        
+        await expect(newUserSignUpText).toHaveText("New User Signup!");
 
         //Enter name and email address, click 'Signup' button, and verify that 'ENTER ACCOUNT INFORMATION' is visible
-        const name = 'Jane Smith';
-        const emailAddress = 'email@emailq.com';
+        const name = "Jane Smith";
+        const emailAddress = "email@jwo0.com";
         const registerNameField = await $('//input[@data-qa="signup-name"]');
         const registerEmailField = await $('//input[@data-qa="signup-email"]');
-        const signupBtn = await $('button=Signup');
-        const registerTitle =await $$('.login-form h2')[0];
-
-      
+        const signupBtn = await $("button=Signup");
+        const registerTitle = await $$(".login-form h2")[0];
 
         await registerNameField.setValue(name);
         await registerEmailField.setValue(emailAddress);
         await signupBtn.click();
-        await expect(registerTitle).toHaveText('ENTER ACCOUNT INFORMATION');
-
-
-
-
+        await expect(registerTitle).toHaveText("ENTER ACCOUNT INFORMATION");
 
         //Fill details: Title, Name, Email, Password, Date of birth, and select checkbox 'Sign up for our newsletter!',
         //Select checkbox 'Receive special offers from our partners!',
@@ -44,56 +37,65 @@ describe("Test Case 1", () => {
         //Click 'Create Account' button,
         ///Veridy that 'ACCOUNT CREATED!' is visible
 
-       
-        
-        const titleMrsRadio = await $('label[@for="id_gender2"]');
-        const passwordField = await $('input[@data-qa="password"]');
-        const password = 'password';
-        const dobDayField = await $('input[@data-qa="days"]');
-        const dobMonthField = await $('input[@data-qa="months"]');
-        const dobYearField = await $('input[@data-qa="years"]');
-        const newsletterCheckbox = await $('#newsletter');
-        const specialofferCheckbox = await $('#optin');
-        const firstNameField = await $('#first_name');
-        const lastNameField = await $('#last-name');
-        const companyField = await $('#company');
-        const address1Field = await $('#address1');
-        const address2Field = await $('#address2');
-        const countryField = await $('#country');
-        const stateField =  await $('#state');
-        const cityField =  await $('#city');
-        const zipcodeField = await $('#zipcode');
-        const mobileNumField = await $('#mobile_number');
-        const createAccountBtn = $('button=Create Account');
-        const accountCreateSuccessMsg= '';
-        const accountCreatedUrl = 'https://automationexercise.com/account_created';
-        const accountCreatedSuccessMsg = $('h2[@data-qa="account_created"]');
+        const titleMrsRadio = await $('//label[@for="id_gender2"]');
+        const passwordField = await $('//input[@data-qa="password"]');
+        const password = "password";
+        const dobDayField = await $('//select[@data-qa="days"]');
+        const dobMonthField = await $('//select[@data-qa="months"]');
+        const dobYearField = await $('//select[@data-qa="years"]');
+        const newsletterCheckbox = await $("#newsletter");
+        const specialofferCheckbox = await $("#optin");
+        const firstNameField = await $("#first_name");
+        const lastNameField = await $("#last_name");
+        const companyField = await $("#company");
+        const address1Field = await $("#address1");
+        const address2Field = await $("#address2");
+        const countryField = await $("#country");
+        const stateField = await $("#state");
+        const cityField = await $("#city");
+        const zipcodeField = await $("#zipcode");
+        const mobileNumField = await $("#mobile_number");
+        const createAccountBtn = $("button=Create Account");
+        const accountCreatedSuccessText = "ACCOUNT CREATED!";
+        const accountCreatedUrl = "https://www.automationexercise.com/account_created";
+        const accountCreatedSuccessMsg = $('//h2[@data-qa="account-created"]');
 
         const user = {
-            firstName: 'Jane',
-            lastName: 'Doe',
-            company: 'ABC',
-            address1: '123 Mark Street',
-            address2: ' Apt 90',
-            country: 'United States',
-            state: 'New York',
-            city: 'New York',
-            zipcode: '456789',
-            mobileNum: '1234567890'
+            firstName: "Jane",
+            lastName: "Doe",
+            company: "ABC",
+            address1: "123 Mark Street",
+            address2: " Apt 90",
+            country: "United States",
+            state: "New York",
+            city: "New York",
+            zipcode: "456789",
+            mobileNum: "1234567890",
         };
-        
-        
 
-
-
-        await titleMrsRadio.isSelected();
+        await titleMrsRadio.click();
         await passwordField.setValue(password);
-        await dobDayField.setValue('12');
-        await dobMonthField.setvalue('April');
-        await dobYearField.setValue('1900');
-        // For loop for checkboxes
-        await newsletterCheckbox.isChecked();
-
+        await dobDayField.selectByVisibleText('12');
+        await dobMonthField.selectByVisibleText("April");
+        await dobYearField.selectByVisibleText("1900");
+        
+        await newsletterCheckbox.click();
+        await specialofferCheckbox.click();
+        await firstNameField.setValue(user.firstName);
+        await lastNameField.setValue(user.lastName);
+        await companyField.setValue(user.company);
+        await address1Field.setValue(user.address1);
+        await address2Field.setValue(user.address2);
+        await countryField.selectByVisibleText(user.country);
+        await stateField.setValue(user.state);
+        await cityField.setValue(user.state);
+        await zipcodeField.setValue(user.zipcode);
+        await mobileNumField.setValue(user.mobileNum);
+        
+        await createAccountBtn.click();
+        await expect(browser).toHaveUrl(accountCreatedUrl);
+        await expect(accountCreatedSuccessMsg).toBeDisplayed();
+        await expect(accountCreatedSuccessMsg).toHaveText(accountCreatedSuccessText);
 
         
 
@@ -101,56 +103,44 @@ describe("Test Case 1", () => {
 
         //Click 'Countinue' button and Verify that 'Logged in as username' is visible
 
+        const registerContinueBtn = await $('a[data-qa="continue-button"]');
+        const loggedInUser = await $('a > b'); 
+        
+        
+        await registerContinueBtn.click();
+        await expect(loggedInUser).toBeDisplayed()
+        await expect(loggedInUser).toHaveText(name);
+
+
+
         //Click Delete Account' button and verify that 'ACCOUNT DELETED!' is visble and click 'Continue' button
+        const deleteAccountBtn = await $('a[href="/delete_account"]');
+        const accountDeletedMsg = await $('h2[data-qa="account-deleted"]');
+        const continueBtn = await $('a[data-qa="continue-button"]');
+        const accountDeletedText = 'ACCOUNT DELETED!';
+
+        await deleteAccountBtn.click();
+        await expect(accountDeletedMsg).toBeDisplayed();
+        await expect(accountDeletedMsg).toHaveText(accountDeletedText);
+        await continueBtn.click();
+
+
+    });
+
+    it("", async () => {});
+
+    it("", async () => {});
+
+    it("", async () => {});
+
+});
+
+describe ('Test Case 2', () => {
+    it('Should log user in with correct email and password', async() => {
         
 
 
-
-
     });
 
-    it('', async () => {
 
-
-    });
-
-    it('', async () => {
-
-
-    });
-
-    it('', async () => {
-
-
-    });
-
-    it('shdoa', async () => {
-
-
-    });
-
-    it('shdoa', async () => {
-
-
-    });
-
-    it('shdoa', async () => {
-
-
-    });
-
-    it('shdoa', async () => {
-
-
-    });
-
-    it('shdoa', async () => {
-
-
-    });
-
-    it('shdoa', async () => {
-
-
-    });
 });
